2c2e2f284fa6fa987fec97b39d1378ad295e6e3b
Tajx <1135854336@qq.com>
2021-11-14 20:29:27 +0800

    完成子界面tabbar的实现

---
+++ wx/custom-tab-bar/tabbar.js
@@ -0,0 +1,64 @@
+// custom-tab-bar/tabbar.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    idx: {
+        type: Number
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    tabBar: [
+        {
+            "pagePath": "../fakeHomepage/fakeHomepage",
+            "text": "首页",
+            "iconPath": "../icons/首页-0.png",
+            "selectedIconPath": "../icons/首页-1.png",
+            "selected": false
+          },
+          {
+            "pagePath": "../article/article",
+            "text": "推送",
+            "iconPath": "../icons/推送-0.png",
+            "selectedIconPath": "../icons/推送-1.png",
+            "selected": false
+          },
+          {
+            "pagePath": "../speak/speak",
+            "text": "说方言",
+            "iconPath": "../icons/说方言-0.png",
+            "selectedIconPath": "../icons/说方言-1.png",
+            "selected": false
+          },
+          {
+            "pagePath": "../fakeMine/fakeMine",
+            "text": "我的",
+            "iconPath": "../icons/我的-0.png",
+            "selectedIconPath": "../icons/我的-1.png",
+            "selected": false
+          }
+    ]
+  },
+  observers: {
+    "idx": function(id) {
+        let otbabar = this.data.tabBar;
+        otbabar[id]['iconPath'] = otbabar[id]['selectedIconPath'];
+        this.setData({tabBar: otbabar});
+    }
+  },
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    navigateDetail : function(e) {
+        wx.reLaunch({
+          url: e.currentTarget.dataset.url
+        })
+    }
+  }
+})
