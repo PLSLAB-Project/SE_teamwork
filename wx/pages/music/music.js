d34d0019ec39fc78824d6db93f5291ad007da794
Tajx <1135854336@qq.com>
2021-11-19 12:09:10 +0800

    完善音乐详情界面

--- wx/pages/music/music.js
+++ wx/pages/music/music.js
@@ -5,22 +5,50 @@ Page({
    * 页面的初始数据
    */
   data: {
-    music:["","","","",""]
+    music: [{
+        title: "浪子的心",
+        auth: "卓依婷"
+      },
+      {
+        title: "用生命所爱的人",
+        auth: "闽南语"
+      },
+      {
+        title: "有梦尚美",
+        auth: "蔡小虎"
+      },
+      {
+        title: "爱到才知痛",
+        auth: "贺一航"
+      },
+      {
+        title: "喜欢就好",
+        auth: "林玉英"
+      }
+    ],
+    renderImgUrl: [
+      "https://cdn.jsdelivr.net/gh/Taj-x/images@main/img/首页-矩形框图片1.png",
+      "https://cdn.jsdelivr.net/gh/Taj-x/images@main/img/首页-矩形框图片2.png",
+      "https://cdn.jsdelivr.net/gh/Taj-x/images@main/img/首页-矩形框图片3.png",
+      "https://cdn.jsdelivr.net/gh/Taj-x/images@main/img/首页-矩形框图片4.png"
+    ]
   },
+  toPlay: function() {
+    wx.navigateTo({
+      url: '../musicPlayer/musicPlayer',
+    })
+  },
+  toArticle: function () {
+      wx.navigateBack({
+        delta: 1
+      });
 
+    },
   /**
    * 生命周期函数--监听页面加载
    */
-  toArticle: function() {
-    wx.navigateBack({
-      delta: 1
-    });
-      
-  },
-  onLoad: function (options) {
-    //let tabbar = this.selectComponent("#tabbar");
-    //tabbar.run();
-  },
+  
+  onLoad: function (options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
