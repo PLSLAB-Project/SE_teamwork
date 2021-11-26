af7a2581e241bdcebe87cca50ea3119e26e7bc39
fzuycy <2253708742@qq.com>
2021-11-26 16:51:21 +0800

    Update speak.js

--- wx/pages/speak/speak.js
+++ wx/pages/speak/speak.js
@@ -1,11 +1,16 @@
 // pages/speak/speak.js
+
+const recorderManager = wx.getRecorderManager();
+const innerAudioContext = wx.createInnerAudioContext();
+let audioPath='';
+
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        isActive:true
     },
 
     /**
@@ -14,84 +19,114 @@ Page({
     onLoad: function (options) {
 
     },
-    InterfaceTest(e){
-        console.log("hi");
-        // let xmlhttp;
-        // if (window.XMLHttpRequest)
-        // {
-        //     //  IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
-        //     xmlhttp=new XMLHttpRequest();
-        // }
-        // else
-        // {
-        //     // IE6, IE5 浏览器执行代码
-        //     xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-        // }
-        // xmlhttp.onreadystatechange=function()
-        // {
-        //     if (xmlhttp.readyState==4 && xmlhttp.status==200)
-        //     {
-        //         console.log( xmlhttp.responseText);
+
+    //开始录音的时候
+    start: function () {
+
+        this.setData({
+            isActive:false
+        })
+        const options = {
+        duration: 10000,//指定录音的时长，单位 ms
+        sampleRate: 16000,//采样率
+        numberOfChannels: 1,//录音通道数
+        encodeBitRate: 96000,//编码码率
+        format: 'wav',//音频格式，有效值 aac/mp3
+        frameSize: 50,//指定帧大小，单位 KB
+        }
+        //开始录音
+        recorderManager.start(options);
+        recorderManager.onStart(() => {
+        console.log('recorder start')
+        });
+        //错误回调
+        recorderManager.onError((res) => {
+        console.log(res);
+        })
+    },
+     //停止录音
+     stop: function () {
+
+        this.setData({
+            isActive:true
+        })
+        recorderManager.stop();
+        recorderManager.onStop((res) => {
+            this.tempFilePath = res.tempFilePath;
+            console.log('停止录音', res.tempFilePath)
+            const { tempFilePath } = res
+            audioPath=res.tempFilePath;
+
+        //     if((typeof res.tempFilePath==='string' )){
+        //         console.log("yes");
+        //     }else{
+        //         console.log("no");
         //     }
-        // }
-        // xmlhttp.open("POST","http://172.17.173.97:8080/api/user/login",true);
-        // xmlhttp.send("student_id=031902522&password=ycyysjyjn13");
-        let that = this 
-        wx.request({
-        method: 'POST',
-        url: 'http://101.43.7.157:8000/alwaysRight/kongoREG', 
-        // url: "https://tcb-api.tencentcloudapi.com",
-        header: {
-            'content-type': 'application/json' // 默认值
-        },
-        data: {
-            "Username":"0",
-            "Password":"1",
-            "NickName":"140"
-        },
-        //data: 'student_id=031902522&password=ycyysjyjn13',
-        success: function (res) {
-            console.log(res)
- 
-        },
-        fail: function () {
-        console.log("fail")
-        },
-        complete: function () {
-            that.setData({
-            isshow:false
-            })
-        } 
-        })
-    },
-    InterfaceTest1(e){
-        console.log("hi");
-        let that = this 
-        wx.request({
-        method: 'POST',
-        url: 'http://101.43.7.157:8000/alwaysRight/reg', 
-        header: {
-            'content-type': 'application/json' // 默认值
-        },
-        //data: 'student_id=031902522&password=ycyysjyjn13',
-        data: {
-            phone: '19859092005',
-        },
-        success: function (res) {
-            console.log(res)
 
-        },
-        fail: function () {
-        console.log("fail")
-        },
-        complete: function () {
-            that.setData({
-            isshow:false
-            })
-        } 
+        //     console.log("test");
+        //      wx.uploadFile({
+        //         url: 'http://101.43.7.157:8000/alwaysRight/uploadAudio4Score',
+        //         filePath: res.tempFilePath,
+        //         name: 'file',
+        //         header: {
+        //              'content-type': 'multipart/form-data'
+        //         },
+        //         formData:{     
+        //             key: 'audio'//这里是为文件设置上传后的文件名
+                
+        //         },
+                
+        //         success: function (res) {
+        //             console.log(res);
+        //              var str = res.data;
+        //         },
+        //         fail: function (res) {
+        //              console.log(res);
+        //         }
+        //         
+        //     });
+
+
+        //     this.setData({
+        //         mypath: ''+res.tempFilePath
+        //     })
         })
     },
 
+    //播放声音
+    play: function () {
+        innerAudioContext.autoplay = true
+        innerAudioContext.src = this.tempFilePath,
+        innerAudioContext.onPlay(() => {
+        console.log('开始播放')
+        })
+        innerAudioContext.onError((res) => {
+        console.log(res.errMsg)
+        console.log(res.errCode)
+        })
+    },
+
+    uploadAudio:function(){
+         console.log("Test_uploadAudio4Score");
+         wx.uploadFile({
+            url: 'http://101.43.7.157:8000/alwaysRight/uploadAudio4Score',
+            filePath: audioPath,
+            name: 'audio',
+            header: {
+                 'content-type': 'multipart/form-data'
+                    // 'content-type': 'application/x-www-form-urlencoded' 
+            },
+            success: function (res) {
+                console.log(res);
+                 var str = res.data;
+            },
+            fail: function (res) {
+                 console.log(res);
+            }
+            
+        });
+
+    },
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
